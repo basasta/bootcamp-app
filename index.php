89aa4da4cda004b3d67941b4d45e1d505e84cf7f
Alex <alexey_vorobyov@inbox.lv>
2015-02-17 14:52:49 +0200

    Hey I have added a puppy

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-dsfsf
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>GETTING STARTED WITH BRACKETS</title>
+    </head>
+    <body>
+        
+        <h1>LETS STRART IT</h1>
+            <h2>KEEP GOING</h2>
+            <form action="smth.php" method="POST">
+                    <label for="urname"> Your name: </label>
+                        <input type="text" name="name" id="urname"/><br />
+                    <label for="passw"> Your password: </label>
+                        <input type="password" name="passw" id="passw"/> <br />
+                    <button>Log in</button>
+            </form>
+        <a href="#"><img src="http://www.hdpaperz.com/wallpaper/original/black-beautiful-dog-1920x1080-wallpaper-3990.jpg" alt="puppy" /></a>
+      
+    </body>
+</html>
