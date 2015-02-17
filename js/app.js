f683953ab7a946c5f8d4d8c6beead4530e15e4a5
Alex <alexey_vorobyov@inbox.lv>
2015-02-17 17:07:22 +0200

    A few modicifations

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 console.log("hello man");
 var x;
-for (x = 1; x <= 30; x++) {
-    if ((x % 3 === 0) && (x%5 === 0)){
-        console.log("FizzBuzz");}
-    else if (x % 3 === 0){
-    console.log("Fizz");}
-    else if (x % 5=== 0){
-    console.log("Buzz");}
-    else{
-    console.log(x);}
+    for (x = 1; x <= 30; x++) {
+        if ((x % 3 === 0) && (x%5 === 0)){
+            console.log("FizzBuzz");
+        }
+        else if (x % 3 === 0){
+            console.log("Fizz");
+        }
+        else if (x % 5=== 0){
+            console.log("Buzz");
+        }
+        else{
+            console.log(x);
+        }
 }
