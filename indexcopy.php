b5461d2d36e3e76ff6e938e42ccaeb6689d2fda9
Alex <alexey_vorobyov@inbox.lv>
2015-02-17 15:47:08 +0200

    I've just added another index page

---
+++ indexcopy.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <title>GETTING STARTED WITH BRACKETS</title>
+          <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        
+        <h1>LETS STRART IT</h1>
+            <h2>KEEP GOING</h2>
+            <form action="smth.php" method="POST">
+                    <label for="urname"> Your name: </label>
+                        <input type="text" name="name" id="urname"/><br />
+                    <label for="passw"> Your password: </label>
+                        <input type="password" name="passw" id="passw"/> <br />
+                    <button>Log in</button>
+            </form>
+        <a href="index.php" title="GO TO INDEX PAGE" target="_blank"><img src="http://www.hdpaperz.com/wallpaper/original/super-3d-guitar-in-the-sky-1920x1080.jpg" alt="puppy" /></a>
+    </body>
+</html>
