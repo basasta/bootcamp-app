f23f46a0d38f67d8422df8496983467285375f88
Alex <alexey_vorobyov@inbox.lv>
2015-02-20 13:46:14 +0200

    YEA man

--- classes/Circle.php
+++ classes/Circle.php
@@ -6,24 +6,23 @@ namespace Bootcamp\Demo;
 class Circle extends Shape  //Inherits properties from Shape Class!
 {
     public $radius;
-     /** 
-      * Creates a Circle instance
-      * @param float $radius is a Circle radius.
-      * @return void
-      */
-    
-     public function getArea() 
+
+    /**
+     * Declares a function getArea.
+     * @return float.
+     */
+    public function getArea()
     {
-      /**
-       *Declares a function getArea.
-       */
-         return M_PI*(($this->radius)*2);       
+        return M_PI*(($this->radius)**2);
     }
-     public function __construct($Radius)
+
+    /**
+     * Creates a Circle instance
+     * @param float $radius is a Circle radius.
+     * @return void
+     */
+    public function __construct($Radius)
     {
-      /**
-       * Declares a function with parameter $Radius and link it to existing in Parent class variables
-       */
-         $this->radius = $Radius;      
+        $this->radius = $Radius;
     }
 }
--- classes/Shape.php
+++ classes/Shape.php
@@ -3,11 +3,12 @@
 namespace Bootcamp\Demo;
 
 abstract class Shape //abstractly discribes every below written class
-{   
-     /** 
+{
+
+     /**
      * Returns an area of a shape
      * @return float value of Shape area
      */
-    public $perimeter;    
-    public abstract function getArea();//Abstract because of the different area calculation ways
+    public $perimeter;
+    abstract public function getArea();//Abstract because of the different area calculation ways
 }
